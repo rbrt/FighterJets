d2a7398c58e94e1f87804af7f96c6589c4435fb6
Rob Butler <robbutler902@gmail.com>
2015-02-13 19:40:33 -0400

    early follow script

---
+++ FighterJets/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraFollow : MonoBehaviour {
+
+	[SerializeField] protected Transform targetPosition;
+	[SerializeField] protected float followSpeed,
+									 rotateSpeed;
+
+	[SerializeField] protected int frameDelay;
+
+	List<Vector3> positions;
+	List<Quaternion> rotations;
+
+	Vector3 refVector;
+
+	void Awake(){
+		refVector = Vector3.zero;
+		positions = new List<Vector3>();
+		rotations = new List<Quaternion>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (positions.Count > 0){
+			transform.position = positions[0];
+			transform.rotation = rotations[0];
+			//Debug.Log("Set position to " + transform.position);
+			positions.RemoveAt(0);
+			rotations.RemoveAt(0);
+		}
+	}
+
+	public void AddFollowStep(Vector3 pos, Quaternion rot){
+		StartCoroutine(WaitThenAddStep(pos, rot));
+	}
+
+	IEnumerator WaitThenAddStep(Vector3 pos, Quaternion rot){
+		int count = frameDelay;
+		while (count-- > 0){
+			yield return null;
+		}
+
+		positions.Add(pos);
+		rotations.Add(rot);
+	}
+}
---
+++ FighterJets/Assets/Scripts/SendCameraPositions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendCameraPositions : MonoBehaviour {
+
+	[SerializeField] protected CameraFollow cameraFollow;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		cameraFollow.AddFollowStep(transform.position, transform.rotation);
+	}
+}
