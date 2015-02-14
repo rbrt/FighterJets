596bf571425b79a0c8f3278c0dc3e41e1183551d
Rob Butler <robbutler902@gmail.com>
2015-02-14 17:18:52 -0400

    Smooth player controls

---
+++ FighterJets/Assets/Scripts/Editor/EditorStuff.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class EditorStuff : MonoBehaviour {
+
+	[MenuItem("Custom/Print Application.Datapath")]
+	public static void PrintApplicationDataPath(){
+		Debug.Log(Application.dataPath);
+	}
+
+}
--- FighterJets/Assets/Scripts/FighterController.cs
+++ FighterJets/Assets/Scripts/FighterController.cs
@@ -11,7 +11,10 @@ public class FighterController : MonoBehaviour {
 									 minSpeed,
 									 pitchRotation,
 									 rollRotation,
-									 yawRotation;
+									 yawRotation,
+									 maxPitch,
+									 maxRoll,
+									 maxYaw;
 
 	bool accelerating,
 		 decelerating,
@@ -22,49 +25,104 @@ public class FighterController : MonoBehaviour {
 		 pitchingUp,
 		 pitchingDown;
 
-	[SerializeField] protected float currentSpeed;
+	[SerializeField] protected float currentSpeed,
+									 currentPitch,
+									 currentYaw,
+									 currentRoll;
+
+	SafeCoroutine yawCoroutine,
+				  pitchCoroutine,
+				  rollCoroutine;
+
+	IEnumerator Primer(){
+		yield break;
+	}
+
+	void Awake(){
+		yawCoroutine = this.StartSafeCoroutine(Primer());
+		pitchCoroutine = this.StartSafeCoroutine(Primer());
+		rollCoroutine = this.StartSafeCoroutine(Primer());
+	}
 
 	void Update () {
 		// Accelerate
 		if (Input.GetKeyDown(KeyCode.W)){
 			accelerating = true;
 		}
-		// Yaw Left
-		if (Input.GetKeyDown(KeyCode.A)){
-			yawingLeft = true;
-		}
 		// Decelerate
 		if (Input.GetKeyDown(KeyCode.S)){
 			decelerating = true;
 		}
+		// Yaw Left
+		if (Input.GetKeyDown(KeyCode.A)){
+			if (!yawingLeft){
+				if (yawCoroutine.IsRunning){
+					yawCoroutine.Stop();
+				}
+				yawCoroutine = this.StartSafeCoroutine(YawCoroutine(false, -maxYaw));
+			}
+
+			yawingLeft = true;
+		}
 		// Yaw Right
 		if (Input.GetKeyDown(KeyCode.D)){
+			if (!yawingRight){
+				if (yawCoroutine.IsRunning){
+					yawCoroutine.Stop();
+				}
+				yawCoroutine = this.StartSafeCoroutine(YawCoroutine(true, maxYaw));
+			}
+
 			yawingRight = true;
 		}
 		// Roll Left
 		if (Input.GetKeyDown(KeyCode.LeftArrow)){
+			if (!rollingLeft){
+				if (rollCoroutine.IsRunning){
+					rollCoroutine.Stop();
+				}
+				rollCoroutine = this.StartSafeCoroutine(RollCoroutine(true, maxRoll));
+			}
+
 			rollingLeft = true;
 		}
 		// Roll Right
 		if (Input.GetKeyDown(KeyCode.RightArrow)){
+			if (!rollingRight){
+				if (rollCoroutine.IsRunning){
+					rollCoroutine.Stop();
+				}
+				rollCoroutine = this.StartSafeCoroutine(RollCoroutine(false, -maxRoll));
+			}
+
 			rollingRight = true;
 		}
 		// Pitch Down
 		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			if (!pitchingDown){
+				if (pitchCoroutine.IsRunning){
+					pitchCoroutine.Stop();
+				}
+				pitchCoroutine = this.StartSafeCoroutine(PitchCoroutine(true, maxPitch));
+			}
+
 			pitchingDown = true;
 		}
 		// Pitch Up
 		if (Input.GetKeyDown(KeyCode.DownArrow)){
+			if (!pitchingUp){
+				if (pitchCoroutine.IsRunning){
+					pitchCoroutine.Stop();
+				}
+				pitchCoroutine = this.StartSafeCoroutine(PitchCoroutine(false, -maxPitch));
+			}
+
 			pitchingUp = true;
 		}
 
 		// Accelerate
 		if (Input.GetKeyUp(KeyCode.W)){
 			accelerating = false;
-		}
-		// Yaw Left
-		if (Input.GetKeyUp(KeyCode.A)){
-			yawingLeft = false;
 		}
 		// Decelerate
 		if (Input.GetKeyUp(KeyCode.S)){
@@ -73,22 +131,56 @@ public class FighterController : MonoBehaviour {
 		// Yaw Right
 		if (Input.GetKeyUp(KeyCode.D)){
 			yawingRight = false;
+
+			if (yawCoroutine.IsRunning){
+				yawCoroutine.Stop();
+			}
+			yawCoroutine = this.StartSafeCoroutine(YawCoroutine(false, 0));
+		}
+		// Yaw Left
+		if (Input.GetKeyUp(KeyCode.A)){
+			yawingLeft = false;
+			if (yawCoroutine.IsRunning){
+				yawCoroutine.Stop();
+			}
+			yawCoroutine = this.StartSafeCoroutine(YawCoroutine(true, 0));
 		}
 		// Roll Left
 		if (Input.GetKeyUp(KeyCode.LeftArrow)){
 			rollingLeft = false;
+
+			if (rollCoroutine.IsRunning){
+				rollCoroutine.Stop();
+			}
+			rollCoroutine = this.StartSafeCoroutine(RollCoroutine(false, 0));
+
 		}
 		// Roll Right
 		if (Input.GetKeyUp(KeyCode.RightArrow)){
 			rollingRight = false;
+
+			if (rollCoroutine.IsRunning){
+				rollCoroutine.Stop();
+			}
+			rollCoroutine = this.StartSafeCoroutine(RollCoroutine(true, 0));
+
 		}
 		// Pitch Down
 		if (Input.GetKeyUp(KeyCode.UpArrow)){
 			pitchingDown = false;
+
+			if (pitchCoroutine.IsRunning){
+				pitchCoroutine.Stop();
+			}
+			pitchCoroutine = this.StartSafeCoroutine(PitchCoroutine(false, 0));
 		}
 		// Pitch Up
 		if (Input.GetKeyUp(KeyCode.DownArrow)){
 			pitchingUp = false;
+			if (pitchCoroutine.IsRunning){
+				pitchCoroutine.Stop();
+			}
+			pitchCoroutine = this.StartSafeCoroutine(PitchCoroutine(true, 0));
 		}
 
 		if (accelerating){
@@ -97,27 +189,56 @@ public class FighterController : MonoBehaviour {
 		if (decelerating){
 			currentSpeed = Mathf.Max(minSpeed, currentSpeed - deceleration * Time.deltaTime);
 		}
-		if (yawingLeft){
-			transform.Rotate(0, -yawRotation, 0);
-		}
-		if (yawingRight){
-			transform.Rotate(0, yawRotation, 0);
-		}
-		if (rollingLeft){
-			transform.Rotate(0, 0, rollRotation);
-		}
-		if (rollingRight){
-			transform.Rotate(0, 0, -rollRotation);
-		}
-		if (pitchingDown){
-			transform.Rotate(pitchRotation, 0, 0);
-		}
-		if (pitchingUp){
-			transform.Rotate(-pitchRotation, 0, 0);
-		}
+
+		transform.Rotate(currentPitch, currentYaw, currentRoll);
 
 		var pos = transform.position;
 		pos += transform.forward * currentSpeed;
 		transform.position = pos;
+	}
+
+	IEnumerator RollCoroutine(bool increase, float target){
+		if (increase){
+			while (currentRoll < target){
+				currentRoll += rollRotation;
+				yield return null;
+			}
+		}
+		else{
+			while (currentRoll > target){
+				currentRoll -= rollRotation;
+				yield return null;
+			}
+		}
+	}
+
+	IEnumerator PitchCoroutine(bool increase, float target){
+		if (increase){
+			while (currentPitch < target){
+				currentPitch += pitchRotation;
+				yield return null;
+			}
+		}
+		else{
+			while (currentPitch > target){
+				currentPitch -= pitchRotation;
+				yield return null;
+			}
+		}
+	}
+
+	IEnumerator YawCoroutine(bool increase, float target){
+		if (increase){
+			while (currentYaw < target){
+				currentYaw += yawRotation;
+				yield return null;
+			}
+		}
+		else{
+			while (currentYaw > target){
+				currentYaw -= yawRotation;
+				yield return null;
+			}
+		}
 	}
 }
