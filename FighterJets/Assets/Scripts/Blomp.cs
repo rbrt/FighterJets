542ac7cacfb77baaf9b001c4eb7ccf89d9d2eca8
Rob Butler <rob@orpheusinteractive.com>
2015-02-16 17:21:19 -0400

    Put the blomp in the game

---
+++ FighterJets/Assets/Scripts/Blomp.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blomp : MonoBehaviour {
+
+    [SerializeField] protected GameObject leftFan,
+                                          rightFan;
+
+    [SerializeField] protected float moveSpeed;
+
+	void Awake(){
+        this.StartSafeCoroutine(SpinBlompFans());
+    }
+
+    void Update() {
+        var pos = transform.position;
+
+        pos += transform.forward * moveSpeed * Time.deltaTime;
+
+        transform.position = pos;
+    }
+
+    IEnumerator SpinBlompFans(){
+        while (true){
+            leftFan.transform.Rotate(0, -1, 0);
+            rightFan.transform.Rotate(0, 1, 0);
+            yield return null;
+        }
+    }
+}
