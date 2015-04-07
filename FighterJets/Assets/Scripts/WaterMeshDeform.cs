e9f33e132526316fe166ebf4a4f40f36704f2565
Rob Butler <robbutler902@gmail.com>
2015-04-06 22:25:08 -0300

    Made water splash meshes rotate and scale independently and with offsets from one another

--- FighterJets/Assets/Scripts/WaterMeshDeform.cs
+++ FighterJets/Assets/Scripts/WaterMeshDeform.cs
@@ -6,7 +6,16 @@ public class WaterMeshDeform : MonoBehaviour {
 	[SerializeField] protected GameObject meshA;
 	[SerializeField] protected GameObject meshB;
 
+    float minScale = .7f,
+          maxScale = 1;
+
 	void Start () {
+        meshA.transform.Rotate(Vector3.up, Random.Range(0, 180));
+        meshB.transform.Rotate(Vector3.up, Random.Range(0, 180));
+
+        meshA.transform.localScale = Random.Range(minScale, maxScale) * Vector3.one;
+        meshB.transform.localScale = Random.Range(minScale, maxScale) * Vector3.one;
+
 		this.StartSafeCoroutine(BoostUp());
 		this.StartSafeCoroutine(PingPongScale(meshA));
 		this.StartSafeCoroutine(PingPongScale(meshB));
@@ -27,12 +36,12 @@ public class WaterMeshDeform : MonoBehaviour {
 		float duration = Random.Range(.2f, .4f);
 		yield return new WaitForSeconds(Random.Range(0,.2f));
 		while (true){
-			for (float i = .7f; i <= 1; i += Time.deltaTime / duration){
+            for (float i = minScale; i <= maxScale; i += Time.deltaTime / duration) {
 				toScale.transform.localScale = Vector3.one * i;
 				yield return null;
 			}
 			toScale.transform.localScale = Vector3.one;
-			for (float i = 1; i >= .7f; i -= Time.deltaTime / duration){
+            for (float i = maxScale; i >= minScale; i -= Time.deltaTime / duration) {
 				toScale.transform.localScale = Vector3.one * i;
 				yield return null;
 			}
@@ -40,3 +49,5 @@ public class WaterMeshDeform : MonoBehaviour {
 		}
 	}
 }
+
+
--- FighterJets/Assets/Scripts/WaterSplashController.cs
+++ FighterJets/Assets/Scripts/WaterSplashController.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 public class WaterSplashController : MonoBehaviour {
 
